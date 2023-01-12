859406cd9075d98d25974c04ceec96580d308dac
Ellen Wang <claire19900125@gmail.com>
2023-01-12 08:21:18 +0800

    completed first part of lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,6 +1,13 @@
 public class HelloNumbers {
     public static void main(String[] args) {
-        System.out.println("hello numbers haha");
+
+        int sum = 0;
+        for (int i = 0; i < 10; i++){
+            sum += i;
+            System.out.println(sum);
+            
+        }
+        
         
     }
 }
